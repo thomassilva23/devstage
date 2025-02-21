c1c0390e3f194643e453c6d7b20b2635ee7b7181
Thomas Silva <thomas@allto.digital>
2025-02-21 11:20:36 -0300

    Criação da página de convite; Atualização de estrutura das páginas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,69 +7,67 @@ import { Button } from "@/components/button";
 
 export default function Home() {
   return (
-    <main className="max-w-[1240px] mx-auto px-5 py-8 md:py-0">
-      <div className="min-h-dvh flex justify-center gap-16 flex-col">
-        <div className="flex flex-col gap-8 items-center md:items-start">
-          <Image src={logo} alt="devstage" width={108.5} height={30} />
+    <div className="min-h-dvh flex justify-center gap-16 flex-col">
+      <div className="flex flex-col gap-8 items-center md:items-start">
+        <Image src={logo} alt="devstage" width={108.5} height={30} />
 
-          <h1 className="text-4xl text-center leading-none font-heading font-medium flex flex-col md:text-7xl md:text-left">
-            <span className="text-blue">CodeCraft</span>
-            Sumit 2025
-          </h1>
-        </div>
-
-        <div className="flex gap-5 items-stretch flex-col md:flex-row">
-          <div className="flex-1 bg-gray-700 border-gray-600 roudned-2xl p-8 space-y-6">
-            <div className="flex items-center justify-between">
-              <h2 className="font-heading font-semibold text-gray-200 text-xl">
-                Sobre o evento
-              </h2>
-              <span className="text-purple font-semibold font-xs flex items-center gap-2">
-                <Radio className="size-5" />
-                AO VIVO
-              </span>
-            </div>
-            <p className="text-gray-300 leading-relaxed text-sm md:text-base">
-              Um evento feito por e para pessoas desenvolvedoras apaixonadas por
-              criar soluções inovadoras e compartilhar conhecimento. Vamos
-              mergulhar nas tendências mais recentes em desenvolvimento de
-              software, arquitetura de sistemas e tecnologias emergentes, com
-              palestras, workshops e hackatons.
-              <br />
-              <br />
-              Dias 15 a 17 de março | Das 18h às 21h | Online & Gratuito
-            </p>
-          </div>
-          <form className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]">
-            <h2 className="font-heading font-semibold text-gray-200 text-xl">
-              Inscrição
-            </h2>
-
-            <div className="space-y-3">
-              <InputRoot>
-                <InputIcon>
-                  <User />
-
-                  <InputField type="text" placeholder="Nome completo" />
-                </InputIcon>
-              </InputRoot>
-
-              <InputRoot>
-                <InputIcon>
-                  <Mail />
-
-                  <InputField type="email" placeholder="E-mail" />
-                </InputIcon>
-              </InputRoot>
-            </div>
-
-            <Button type="submit">
-              Confirmar
-              <ArrowRight />
-            </Button>
-          </form>
-        </div>
+        <h1 className="text-4xl text-center leading-none font-heading font-medium flex flex-col md:text-7xl md:text-left">
+          <span className="text-blue">CodeCraft</span>
+          Sumit 2025
+        </h1>
       </div>
-    </main>
+
+      <div className="flex gap-5 items-stretch flex-col md:flex-row">
+        <div className="flex-1 bg-gray-700 border-gray-600 roudned-2xl p-8 space-y-6">
+          <div className="flex items-center justify-between">
+            <h2 className="font-heading font-semibold text-gray-200 text-xl">
+              Sobre o evento
+            </h2>
+            <span className="text-purple font-semibold font-xs flex items-center gap-2">
+              <Radio className="size-5" />
+              AO VIVO
+            </span>
+          </div>
+          <p className="text-gray-300 leading-relaxed text-sm md:text-base">
+            Um evento feito por e para pessoas desenvolvedoras apaixonadas por
+            criar soluções inovadoras e compartilhar conhecimento. Vamos
+            mergulhar nas tendências mais recentes em desenvolvimento de
+            software, arquitetura de sistemas e tecnologias emergentes, com
+            palestras, workshops e hackatons.
+            <br />
+            <br />
+            Dias 15 a 17 de março | Das 18h às 21h | Online & Gratuito
+          </p>
+        </div>
+        <form className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]">
+          <h2 className="font-heading font-semibold text-gray-200 text-xl">
+            Inscrição
+          </h2>
+
+          <div className="space-y-3">
+            <InputRoot>
+              <InputIcon>
+                <User />
+
+                <InputField type="text" placeholder="Nome completo" />
+              </InputIcon>
+            </InputRoot>
+
+            <InputRoot>
+              <InputIcon>
+                <Mail />
+
+                <InputField type="email" placeholder="E-mail" />
+              </InputIcon>
+            </InputRoot>
+          </div>
+
+          <Button type="submit">
+            Confirmar
+            <ArrowRight />
+          </Button>
+        </form>
+      </div>
+    </div>
   );
 }
