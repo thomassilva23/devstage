07c4ab29ff0cf9fc1a236ea156fda7036a47d8a1
Thomas Silva <thomas@allto.digital>
2025-02-22 19:29:54 -0300

    Configuração Orval para backend; Ajuste de página para convite;

--- src/app/invite/[subscriberId]/page.tsx
+++ src/app/invite/[subscriberId]/page.tsx
@@ -2,10 +2,17 @@ import Image from "next/image";
 import { Ranking } from "./ranking";
 import { Stats } from "./stats";
 import { InviteLinkInput } from "./invite-link-input";
-import logo from "../../assets/logo.svg";
+import logo from "../../../assets/logo.svg";
 
-export default function InvitePage() {
-  const inviteLink = "http://localhost:3000/invite/129823162";
+interface InvitePageProps {
+  params: Promise<{
+    subscriberId: string;
+  }>;
+}
+
+export default async function InvitePage(props: InvitePageProps) {
+  const { subscriberId } = await props.params;
+  const inviteLink = `http://localhost:3333/invites/${subscriberId}`;
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
       <div className="flex flex-col gap-10 w-full max-w-[550px]">
@@ -36,7 +43,7 @@ export default function InvitePage() {
 
           <InviteLinkInput inviteLink={inviteLink} />
 
-          <Stats />
+          <Stats subscriberId={subscriberId} />
         </div>
       </div>
 
